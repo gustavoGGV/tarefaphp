7daf6fd905e8a0eceaf6aec3d6ffa33d592e91ac
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-02-23 10:23:23 -0300

    Create tarefa2302.php

---
+++ tarefa2302.php
@@ -0,0 +1,11 @@
+<?php
+
+$n=readline("Digite um número: ");
+$m=readline("Digite o multiplicador deste número: ");
+$soma=$n;
+
+for($i=1;$i<$m;$i++){
+    $n+=$soma;
+}
+
+print "\nResultado: " . $n . "\n\n";
