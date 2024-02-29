dd01d0eec2bece99847b3bb7843e93174f3cd800
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-02-29 15:12:23 -0300

    Create arrayso.php

---
+++ arrayso.php
@@ -0,0 +1,23 @@
+<?php
+
+$nomes=array("Gabriel", "Henrique", "Barbosa", "de Arrascaeta", "da Silva");
+$times=array("Flamengo", "Vasco", "Fluminense", "Botafogo", "Bangu");
+$numeros=array(1, 2, 3, 4, 5);
+$formula=array("Ferrari", "McLaren", "Williams", "Mercedes", "Red Bull");
+
+foreach($nomes as $i){
+    print($i . " | ");
+}
+print "\n";
+foreach($times as $x){
+    print($x . " | ");
+}
+print "\n";
+foreach($numeros as $y){
+    print($y . " | ");
+}
+print "\n";
+foreach($formula as $z){
+    print($z . " | ");
+}
+print "\n";
